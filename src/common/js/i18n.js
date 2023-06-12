f370dd1d2ccc2c878c93216e579aaf40898b0966
Pierre Wieser <pwieser@trychlos.org>
2023-06-12 13:31:40 +0200

    Update to new pwixI18n api

--- src/common/js/i18n.js
+++ src/common/js/i18n.js
@@ -8,4 +8,4 @@ import { pwixI18n as i18n } from 'meteor/pwix:i18n';
 //i18n.set( FRSI18N, 'en_US', pwiForums.i18n.en_US );
 
 import '../i18n/fr_FR.js';
-i18n.set( FRSI18N, 'fr_FR', pwiForums.i18n.fr_FR );
+i18n.namespace( FRSI18N, 'fr_FR', pwiForums.i18n.fr_FR );
